f586643a84aff674eb3f75b34c32b67d06b6fb41
Vlad Filippov <vlad.filippov@gmail.com>
2016-11-29 12:05:56 -0500

    Daily builds (#7)

--- setup.sh
+++ setup.sh
@@ -1,32 +1,41 @@
 #!/bin/bash -ex
 
 rm -rf ./firefox
+rm -rf ./venv_modules
 
-DATE=`date +%Y-%m-%d`
-
+# Always stay one day behind Nightly to avoid 404ing on the download
 # Create virtualenv
 virtualenv venv_modules
 
 # Activate virtualenv
 source venv_modules/bin/activate
 
+pip install -U pip
+
 # Install modules to download Firefox
 pip install -r requirements.txt --no-cache-dir
 
 # Download Firefox
-mozdownload --type=daily --date=$DATE --destination=firefox
+mozdownload --type=daily --destination=firefox
 
 # Install Firefox into our directory
-mozinstall firefox/$DATE-* -d firefox
+mozinstall firefox/*firefox* -d firefox
 
 # Download common test files
-mozdownload --type=daily --date=$DATE --extension common.tests.zip --destination=firefox
+mozdownload --type=daily --extension common.tests.zip --destination=firefox
 
 # Unzip the common tests
 unzip -q firefox/*.common.tests.zip -d firefox/tests
 
 # Install Marionette requirements
-cd firefox/tests/config && pip install -r firefox_ui_requirements.txt --no-cache-dir
+cd firefox/tests/config && pip install -r firefox_ui_requirements.txt
 cd ../../..
+
+# Replace firefox_puppeteer
+# For some reason PIP installs the old version of firefox_puppeteer
+# Replace the installed version with the one provided by common.tests.zip
+rm -rf ./venv_modules/lib/python*/site-packages/firefox_puppeteer
+cp -r firefox/tests/puppeteer/firefox/firefox_puppeteer ./venv_modules/lib/python*/site-packages/
+
 
 echo "Dependencies installed. Run ./run.sh to run the tests..."
